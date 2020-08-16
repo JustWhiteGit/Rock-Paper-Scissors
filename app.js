89983f496bebf1a16875416801f31e87220194a4
J White <jmw-81@hotmail.co.uk>
2020-08-16 22:01:32 +0000

    created functions to call random values to act as a comparrison with the users selection

--- app.js
+++ app.js
@@ -5,15 +5,48 @@
 const userScore = 0;
 const computerScore = 0;
 const userScore_span = document.getElementById("user-score");
-const computerScore_span = document.getElementById("computer-score"); 
+const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
 const result_div = document.querySelector(".result");
 const rock_div = document.getElementById("r");
-const paper = document.getElementById("p");
-const scissors = document.getElementById("s");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+
+/* this function takes the users choice/selection - when user clicks, take the value of the button (be it r, p or s) and then compare it agaisnt the computers choice and then get result back*/
+
+function getComputerChoice() {
+    const choices = ['r', 'p', 's'];
+    const randomNumber = Math.floor(Math.random() * 3); /* Math is built in object in js and randome exists in that object */
+    return choices[randomNumber];
+}
+/* console.log(getComputerChoice()); -- now that we know this works we can remove this from the code - but kept here for reminder */
+
+
+function game(userChoice) {
+  const computerChoice = getComputerChoice();
+  console.log("user choice = " + userChoice);
+  console.log("computer choice = " + computerChoice); 
+}
 
 /* He explains we need event listeners now to run the comparisons of the game choices of rock/paper/scissors. When we click on an image it will take that value and compare with the computers choice between the same three. then see whpo wins and display as a score. */
 
-rock_div.addEventListener('click', function(){
-    console.log("hey, you clicked on rock");
-})
+function main() {
+  rock_div.addEventListener("click", function () {
+    /* console.log("hey, you clicked on rock"); note: console logged to show evidence that button was clicked.  */
+    game("r");
+  });
+
+  paper_div.addEventListener("click", function () {
+    /*console.log("hey, you clicked on paper");*/
+    game("p");
+  });
+
+  scissors_div.addEventListener("click", function () {
+    /*console.log("hey, you clicked on scissors");*/
+    game("s");
+  });
+  
+}
+
+
+main();
